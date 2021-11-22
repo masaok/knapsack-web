6e9ddd67d47236906863666c2528e6eb652cc1d1
Masao Kitamura <masaok@gmail.com>
2021-11-22 10:49:47 -0800

    init

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,79 @@
+// import { useState, useEffect } from 'react'
+import { makeStyles } from '@mui/styles'
+
+import { Helmet } from 'react-helmet-async'
+
+// import EmptyContent from './content/EmptyContent/EmptyContent'
+
+// import { Routes, Route } from 'react-router-dom'
+
+// import logoTrans from '../../assets/images/placeholder-logo.png'
+
+const useStyles = makeStyles(
+  theme => ({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      backgroundColor: 'pink',
+      height: '100vh',
+    },
+
+    header: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+
+    body: {
+      display: 'flex',
+    },
+
+    sidebar: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+
+    content: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+
+    // Logo
+    logoContainer: {
+      display: 'flex',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      maxWidth: 160,
+    },
+
+    logo: {
+      width: '100%',
+      height: 'auto',
+    },
+  }),
+  { name: 'Dashboard' }
+)
+
+const Dashboard = props => {
+  const classes = useStyles(props)
+
+  return (
+    <div className={classes.root}>
+      <Helmet>
+        <title>Dashboard</title>
+      </Helmet>
+      <div className={classes.header}>
+        {/* <div className={classes.logoContainer}>
+          <img className={classes.logo} src={logoTrans} alt="knapsack visualizer logo" />
+        </div> */}
+        Knapsack Visualizer
+      </div>
+      <div className={classes.body}>
+        <div className={classes.sidebar}>SIDEBAR</div>
+        <div className={classes.content}>CONTENT</div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
