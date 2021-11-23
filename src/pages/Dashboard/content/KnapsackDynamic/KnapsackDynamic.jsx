caefed960de730f05c184b0f726d2b80e6b8dfb4
Masao Kitamura <masaok@gmail.com>
2021-11-23 11:54:30 -0800

    Knapsack algo adds to a table correctly and the table dumps correctly in the console with a 2 x 2 table

--- src/pages/Dashboard/content/KnapsackDynamic/KnapsackDynamic.jsx
+++ src/pages/Dashboard/content/KnapsackDynamic/KnapsackDynamic.jsx
@@ -1,3 +1,4 @@
+import { useState, useCallback, useEffect } from 'react'
 import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles(
@@ -29,30 +30,98 @@ const dump = (maxWeight, weights, values, numItems, depth) => {
   console.log('WEIGHTS:' + JSON.stringify(weights))
 }
 
+const tableDump = table => {
+  console.log(JSON.stringify(table, null, 2))
+}
+
+// const NUM_ITEMS = VALUES.length
+
+const table = [[]]
+
 const KnapsackDynamic = props => {
   const classes = useStyles(props)
 
-  // Returns the maximum value that can
-  // be put in a knapsack of capacity maxWeight
-  const knapsack = (maxWeight, weights, values, numItems, depth = 0) => {
-    let i, w
-    let table = new Array(numItems + 1)
+  const [seconds, setSeconds] = useState(0)
+  const [row, setRow] = useState(0)
+  const [col, setCol] = useState(0)
 
-    // Build table table[][] in bottom up manner
-    for (i = 0; i <= numItems; i++) {
-      table[i] = new Array(maxWeight + 1)
-      for (w = 0; w <= maxWeight; w++) {
-        if (i === 0 || w === 0) table[i][w] = 0
-        else if (weights[i - 1] <= w)
-          table[i][w] = max(values[i - 1] + table[i - 1][w - weights[i - 1]], table[i - 1][w])
-        else table[i][w] = table[i - 1][w]
+  // const [maxRow] = useState(NUM_ITEMS)
+  // const [maxCol] = useState(MAX_WEIGHT)
+
+  const [maxRow] = useState(1) // dev mode
+  const [maxCol] = useState(2) // dev mode
+
+  // const [table, setTable] = useState(new Array(NUM_ITEMS + 1))
+
+  const timer = useCallback(() => {
+    console.log(`S: ${seconds}, ROW: ${row}, COL: ${col}`)
+    tableDump(table)
+    if (col < maxCol) {
+      console.log(`CREATING NEW COL AT ROW ${row} AND COL ${col}`)
+
+      if (row === 0 || col === 0) {
+        // table[row].push(`ROW ${row} COL ${col}`)
+        table[row].push(0)
+      } else if (WEIGHTS[row - 1] <= col) {
+        table[row].push(
+          max(VALUES[row - 1] + table[row - 1][col - WEIGHTS[row - 1]], table[row - 1][col])
+        )
+      } else {
+        table[row].push(table[row - 1][col])
       }
+
+      setCol(col + 1)
+      setSeconds(seconds + 1)
+
+      // console.log(table)
+    } else if (row < maxRow) {
+      // table[row] = new Array(MAX_WEIGHT + 1)
+      // setTable(table => {
+      //   table[row] = new Array(MAX_WEIGHT + 1)
+      //   return table
+      // })
+
+      console.log(`CREATING NEW ROW AT ${row}`)
+      table.push([])
+      setRow(row + 1)
+      setCol(0)
+      setSeconds(seconds + 1)
     }
 
-    console.log(table)
+    // If neither condition is true, the state doesn't change, which doesn't
+    // recreate the timer, which doesn't trigger the useEffect. The loop
+    // will stop.
+  }, [maxCol, maxRow, seconds, row, col])
 
-    return table[numItems][maxWeight]
-  }
+  useEffect(() => {
+    const timeout = setTimeout(timer, 1000)
+
+    // Make sure to clear the current timeout whenever a new timer is generated.
+    // This ensures that only one timeout is active at a time.
+    return () => clearTimeout(timeout)
+  }, [timer])
+
+  // Returns the maximum value that can
+  // be put in a knapsack of capacity maxWeight
+  // const knapsack = (maxWeight, weights, values, numItems, depth = 0) => {
+  //   let i, w
+  //   let table = new Array(numItems + 1)
+
+  //   // Build table table[][] in bottom up manner
+  //   for (i = 0; i <= numItems; i++) {
+  //     table[i] = new Array(maxWeight + 1)
+  //     for (w = 0; w <= maxWeight; w++) {
+  //       if (i === 0 || w === 0) table[i][w] = 0
+  //       else if (weights[i - 1] <= w)
+  //         table[i][w] = max(values[i - 1] + table[i - 1][w - weights[i - 1]], table[i - 1][w])
+  //       else table[i][w] = table[i - 1][w]
+  //     }
+  //   }
+
+  //   console.log(table)
+
+  //   return table[numItems][maxWeight]
+  // }
 
   console.log('STARTING HERE:')
   dump(MAX_WEIGHT, WEIGHTS, VALUES, VALUES.length, 0)
@@ -60,7 +129,8 @@ const KnapsackDynamic = props => {
   console.log('RETURNING:')
   return (
     <div className={classes.root}>
-      DYNAMIC VIS HERE: {knapsack(MAX_WEIGHT, WEIGHTS, VALUES, VALUES.length)}
+      DYNAMIC VIS HERE:
+      {/* {knapsack(MAX_WEIGHT, WEIGHTS, VALUES, VALUES.length)} */}
     </div>
   )
 }
